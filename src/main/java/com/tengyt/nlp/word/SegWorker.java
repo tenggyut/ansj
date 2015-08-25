1a46f45631799debaf99e037e54a4f010df7f0f7
tenggyut <tengyutong0213@gmail.com>
2015-08-25 23:40:05 +0800

    add worker logic

--- src/main/java/com/tengyt/nlp/word/SegWorker.java
+++ src/main/java/com/tengyt/nlp/word/SegWorker.java
@@ -1,28 +1,41 @@
 package com.tengyt.nlp.word;
 
 import com.google.common.collect.Queues;
+import com.google.common.collect.Sets;
 import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.NlpAnalysis;
 
-import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.Callable;
+import java.util.Set;
 
 /**
+ * seg worker
+ *
  * Created by tengyt on 8/25/15.
  */
-public class SegWorker implements Callable<List<Term>>{
+public class SegWorker implements Runnable {
     public static Queue<Email> emails = Queues.newConcurrentLinkedQueue();
+    public static Set<String> terms = Sets.newConcurrentHashSet();
+
+    private int idealCount = 0;
 
     @Override
-    public List<Term> call() throws Exception {
-        while (true) {
+    public void run() {
+        while (idealCount < 200) {
             if (emails.isEmpty()) {
-                Thread.sleep(1000);
+                idealCount++;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 continue;
             }
 
             Email email = emails.poll();
+            for (Term term : NlpAnalysis.parse(email.getContent())) {
+                terms.add(term.toString());
+            }
         }
-        return null;
     }
 }
