43cfb5c056150dcfd9cc815df6d57102364f7398
tenggyut <tengyutong0213@gmail.com>
2015-08-26 22:39:11 +0800

    finish seg logic

---
+++ src/main/java/com/tengyt/nlp/word/SegExecutor.java
@@ -0,0 +1,7 @@
+package com.tengyt.nlp.word;
+
+/**
+ * Created by tengyt on 8/26/15.
+ */
+public class SegExecutor {
+}
---
+++ src/test/java/com/tengyt/nlp/word/SegExecutorTest.java
@@ -0,0 +1,14 @@
+package com.tengyt.nlp.word;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tengyt on 8/26/15.
+ */
+public class SegExecutorTest {
+
+    @org.junit.Test
+    public void testDoWork() throws Exception {
+
+    }
+}
